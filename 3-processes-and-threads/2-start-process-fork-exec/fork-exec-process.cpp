dc64f57e3ae2f3c510954c2eb74b88ecce325b6f
stardvst <lilia.manukyan@hotmail.com>
2026-02-06 23:32:03 +0400

    fork+execing a child process

---
+++ 3-processes-and-threads/2-start-process-fork-exec/fork-exec-process.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+	pid_t child;
+	int status;
+
+	std::cout << "I'm the parent process, my pid is: " << getpid() << std::endl;
+	std::cout << "My parent process's pid is: " << getppid() << std::endl;
+	std::cout << "I'm going to create a child process..." << std::endl;
+
+	child = fork();
+
+	if (child == -1)
+	{
+		std::cerr << "Failed to fork process" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (child == 0)
+	{
+		std::cout << "  I'm the child process, my pid is: " << getpid() << std::endl;
+		std::cout << "  My parent process's pid is: " << getppid() << std::endl;
+		std::cout << "  I'm going to execute the ls command..." << std::endl;
+
+		if (execlp("ls", "ls", "-l", NULL) == -1)
+		{
+			std::cerr << "Failed to execute ls command" << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	else
+	{
+    // in the parent process, wait for the child to finish
+		wait(&status);
+		std::cout << "Back, I'm the parent process, my pid is still: " << getpid() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
